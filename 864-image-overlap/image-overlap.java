c4129f600fc22b6537c2e917b8a4719b15a5e6bf
Abhinav Chaudhary <abhinavch97@gmail.com>
2026-09-13 19:58:41 +0530

    Time: 29 ms (96.21%) | Memory: 44.7 MB (56.06%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,12 +1,9 @@
 class Solution {
-    /**
-     *  Shift the matrix M in up-left and up-right directions 
-     *    and count the ones in the overlapping zone.
-     */
+    
     protected int shiftAndCount(int xShift, int yShift, int[][] M, int[][] R) {
         int leftShiftCount = 0, rightShiftCount = 0;
         int rRow = 0;
-        // count the cells of ones in the overlapping zone.
+        
         for (int mRow = yShift; mRow < M.length; ++mRow) {
             int rCol = 0;
             for (int mCol = xShift; mCol < M.length; ++mCol) {
@@ -26,9 +23,9 @@ class Solution {
 
         for (int yShift = 0; yShift < A.length; ++yShift)
             for (int xShift = 0; xShift < A.length; ++xShift) {
-                // move the matrix A to the up-right and up-left directions.
+               
                 maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, A, B));
-                // move the matrix B to the up-right and up-left directions, which is equivalent to moving A to the down-right and down-left directions 
+                
                 maxOverlaps = Math.max(maxOverlaps, shiftAndCount(xShift, yShift, B, A));
             }
 
