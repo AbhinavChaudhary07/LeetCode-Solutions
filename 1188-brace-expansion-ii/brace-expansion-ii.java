62e89ba6cdb7c2b3cac48303c91894c3e9110c62
Abhinav Chaudhary <abhinavch97@gmail.com>
2026-09-25 11:53:58 +0530

    Time: 29 ms (15.20%) | Memory: 53.3 MB (6.40%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.java
+++ 1188-brace-expansion-ii/brace-expansion-ii.java
@@ -4,19 +4,19 @@ class Solution {
     void dfs(String s) {
         int r = s.indexOf('}');
 
-        // No braces left
+        
         if (r == -1) {
             ans.add(s);
             return;
         }
 
-        // Find matching '{'
+        
         int l = s.lastIndexOf('{', r);
 
         String left = s.substring(0, l);
         String right = s.substring(r + 1);
 
-        // Content inside { }
+       
         String inside = s.substring(l + 1, r);
 
         for (String part : inside.split(",")) {
